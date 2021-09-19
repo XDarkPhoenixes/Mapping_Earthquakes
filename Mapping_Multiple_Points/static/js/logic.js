8f975143f845dcc2f5154c0bf405fe502a88fb62
XDarkPhoenixes <tronziheng.zhou@mail.utoronto.ca>
2021-09-18 23:22:22 -0400

    Update js and html files

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 
 //  Add a marker/circle to the map for Los Angeles, California.
@@ -13,12 +13,33 @@ let map = L.map('mapid').setView([34.0522, -118.2437], 14);
     //fillColor: "#FFFF99",
 //}).addTo(map);
 
-L.circleMarker([34.0522, -118.2437], {
-    radius: 300,
-    color: 'black',
-    fillColor: '#ffffa1'
-}).addTo(map);
+// L.circleMarker([34.0522, -118.2437], {
+    //radius: 300,
+    //color: 'black',
+    //fillColor: '#ffffa1'
+//}).addTo(map);
 
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+//cityData.forEach(function(city) {
+    //console.log(city);
+    //L.marker(city.location)
+    //.bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>").addTo(map);
+//});
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city);
+    L.circleMarker(city.location, {
+        radius: city.population/100000,
+        color: "orange",
+        weight: 4,
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+});
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
